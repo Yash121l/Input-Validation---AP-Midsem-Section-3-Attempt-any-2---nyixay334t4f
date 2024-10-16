75b283812f785bad8c323f89d6f61fd5dae96a90
Yash121l <yashlunawat121@gmail.com>
2024-10-16 14:55:11 +0530

    coded

--- index.js
+++ index.js
@@ -14,11 +14,16 @@ app.get('/', (req, res) => {
 app.post("/formdata",(req,res)=>{
     const {username,email,password}=req.body;
     if (username.length < 5) {
+        res.status(400).json({"message": "Username must be at least 5 characters long."})
     }
     if (email.length < 5 || !email.includes('@') || !email.includes('.com')) {
+        res.status(400).json({"message": "Email must be at least 5 characters long and must contain both '@' and '.com'."})
     }
     if (password.length < 5) {
+        res.status(400).json({"message": "Password must be at least 5 characters long."})
     }
+
+    res.status(400).json({"message": "Credentials are valid"})
 });
 
 const port = process.env.PORT || 3000;
